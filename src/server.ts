88644a03e7e9d49f9052889722fd4e48fa0610bf
Carlos <carlosestupina@gmail.com>
2024-08-21 19:28:57 +0200

    ejercicio 13

--- src/server.ts
+++ src/server.ts
@@ -10,30 +10,35 @@ let planetas = [...planets]
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/planets', (req: Request, res: Response) => {
-  res.json(planetas);
+app.get('/api/planets', (req: Request, res: Response) => {
+  res.status(200).json(planetas);
 });
-app.get('/planets/:id', (req: Request, res: Response) => {
+app.get('/api/planets/:id', (req: Request, res: Response) => {
   const {id} = req.params;
   const planet = planets.find((p) => p.id === Number(id));
-  res.json(planet);
+  res.status(200).json(planet);
 });
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
-app.post('/planets', (req: Request, res: Response) => {
+app.post('/api/planets', (req: Request, res: Response) => {
   const {id, name} = req.body;
   const newPlanet = {id, name};
  planetas = [...planetas, newPlanet];
  res.status(201).json({msg: "the planet was created", planets: planetas});
 });
-app.put('/planets/:id', (req: Request, res: Response) => {
+app.put('/api/planets/:id', (req: Request, res: Response) => {
   const {id} = req.params;
   const {name} = req.body;
   planetas = planetas.map((p) => p.id === Number(id) ? {...p, name} : p);
   console.log(planetas);
   res.status(200).json({msg: "the planet was updated", planets: planetas})
-  
+});
+app.delete('/api/planets/:id', (req: Request, res: Response) => {
+  const {id} = req.params;
+  planetas = planetas.filter((p) => p.id !== Number(id));
+  console.log(planetas);
+  res.status(200).json({msg: "the planet was deleted", planets: planetas})
 });
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
