99da4f80da09482297e740e970d068bf0d2f5275
Carlos <carlosestupina@gmail.com>
2024-08-21 19:16:37 +0200

    no message

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+
+-------------------------
+
 import express, { Express, Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import morgan from 'morgan';
@@ -6,12 +9,12 @@ import { planets } from './models/planets';
 dotenv.config();
 const app: Express = express();
 const port: number = Number(process.env.PORT ?? 3000);
-
+let planetas = [...planets]
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.get('/planets', (req: Request, res: Response) => {
-  res.json(planets);
+  res.json(planetas);
 });
 app.get('/planets/:id', (req: Request, res: Response) => {
   const {id} = req.params;
@@ -21,7 +24,20 @@ app.get('/planets/:id', (req: Request, res: Response) => {
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
-
+app.post('/planets', (req: Request, res: Response) => {
+  const {id, name} = req.body;
+  const newPlanet = {id, name};
+ planetas = [...planetas, newPlanet];
+ res.status(201).json({msg: "the planet was created", planets: planetas});
+});
+app.put('/planets/:id', (req: Request, res: Response) => {
+  const {id} = req.params;
+  const {name} = req.body;
+  planetas = planetas.map((p) => p.id === Number(id) ? {...p, name} : p);
+  console.log(planetas);
+  res.status(200).json({msg: "the planet was updated", planets: planetas})
+  
+});
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
