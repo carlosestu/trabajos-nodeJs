33fb3d32c74875e025658763c32a95eb652d3f6e
Carlos <carlosestupina@gmail.com>
2024-09-02 19:01:19 +0200

    no message

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -5,13 +5,3 @@ export const getAllUsers = async(req: Request, res: Response) => {
     const users = await db.many(`SELECT * FROM users`);
       res.status(200).json(users);
     };
-  export const logIn = async(req: Request, res: Response) => {
-    const {username, password} = req.body;
-    const user = await db.one(`SELECT * FROM users WHERE username=$1`, username);
-      if (user && user.password === password) {
-        const token =
-        res.status(200).json({msg:"login completado"});
-      } else {
-        res.status(400).json({msg:"credenciales incorrectas"})
-      }
-    };
