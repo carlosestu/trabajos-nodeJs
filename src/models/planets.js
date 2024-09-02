41454543241f86e4491536c3d6c50d2103adb273
Carlos <carlosestupina@gmail.com>
2024-09-02 12:00:16 +0200

    no message

---
+++ src/models/planets.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.planets = void 0;
+exports.planets = [
+    {
+        id: 1,
+        name: "Mercurio",
+    },
+    {
+        id: 2,
+        name: "Venus",
+    },
+    {
+        id: 3,
+        name: "Tierra",
+    },
+    {
+        id: 4,
+        name: "Marte",
+    },
+    {
+        id: 5,
+        name: "Júpiter",
+    },
+    {
+        id: 6,
+        name: "Saturno",
+    },
+    {
+        id: 7,
+        name: "Urano",
+    },
+    {
+        id: 8,
+        name: "Neptuno",
+    }
+];
